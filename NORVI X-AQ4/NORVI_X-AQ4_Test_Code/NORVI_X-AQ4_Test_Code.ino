9813aa6a12c1eef6cf32faebf6146afc5967b216
NORVI-DOCS <pcu3@icd.lk>
2026-08-05 17:47:01 +0530

    Update NORVI_X-AQ4_Test_Code.ino

--- NORVI X-AQ4/NORVI_X-AQ4_Test_Code/NORVI_X-AQ4_Test_Code.ino
+++ NORVI X-AQ4/NORVI_X-AQ4_Test_Code/NORVI_X-AQ4_Test_Code.ino
@@ -1,81 +1,12 @@
+/*
+ * NORVI X-AQ4-v2
+ */
 #include <Wire.h>
-#include <SPI.h>
-#include "TFT_eSPI.h"
-#include "Free_Fonts.h"
 
 // ================= I2C =================
 #define SDA_PIN 8     // Change if needed
 #define SCL_PIN 9
 #define DAC_ADDR 0x5F
-
-// ================= TFT =================
-#define MISO 13
-#define MOSI 11
-#define SCLK 12
-#define DSP_CS 45
-
-TFT_eSPI tft = TFT_eSPI();
-
-// ================= DISPLAY VALUES =================
-int lastChannel = 0;
-float lastValue = 0;
-uint16_t lastDAC = 0;
-bool isVoltage = true;
-
-float val;
-uint16_t dac_value = 0;
-int ch = 0;
-
-
-// =========================================================
-// DRAW HEADER
-// =========================================================
-void drawHeader()
-{
-  tft.fillScreen(TFT_BLACK);
-
-  tft.setTextColor(TFT_YELLOW);
-  tft.setFreeFont(FSB12);
-
-  tft.setCursor(20, 20);
-  tft.print("NORVI X AQ4");
-
-}
-
-
-// =========================================================
-// DRAW VALUES
-// =========================================================
-void drawValues()
-{
-  tft.fillScreen(TFT_BLACK);
-
-  tft.setTextColor(TFT_YELLOW);
-  tft.setFreeFont(FSB9);
-
-  tft.setCursor(20,20);
-  tft.println("NORVI X AQ4");
-
-  tft.setTextColor(TFT_WHITE);
-  tft.setCursor(20,70);
-
-  tft.print("CH");
-  tft.print(lastChannel);
-  tft.print(" = ");
-  tft.print(val,2);
-
-  if(isVoltage)
-    tft.print(" V");
-  else
-    tft.print(" mA");
-
-  tft.setTextColor(TFT_CYAN);
-  tft.setCursor(20,110);
-
-  tft.print("DAC Code = ");
-  tft.println(dac_value);
-}
-
 
 // =========================================================
 void setup() {
@@ -84,16 +15,6 @@ void setup() {
   Wire.begin(SDA_PIN, SCL_PIN);
 
   delay(300);
-
-  SPI.begin(SCLK, MISO, MOSI);
-
-  tft.init();
-  tft.begin();
-  tft.setRotation(0);
-
-  drawHeader();
-
-  delay(1500);
 
   config_dac(DAC_ADDR, 0, 0, 1, 1);
 
@@ -130,7 +51,7 @@ void processCommand(String cmd) {
     return;
   }
 
-  ch = cmd.substring(2, 3).toInt();
+  int ch = cmd.substring(2, 3).toInt();
   int eqIndex = cmd.indexOf('=');
 
   if (ch < 1 || ch > 4 || eqIndex < 0) {
@@ -138,8 +59,8 @@ void processCommand(String cmd) {
     return;
   }
 
-  val = cmd.substring(eqIndex + 1).toFloat();
-  dac_value = 0;
+  float val = cmd.substring(eqIndex + 1).toFloat();
+  uint16_t dac_value = 0;
 
   // -------- CH1 & CH2 (0–10V) --------
   if (ch == 1 || ch == 2) {
@@ -177,7 +98,6 @@ void processCommand(String cmd) {
 
   Serial.print(" | DAC Code = ");
   Serial.println(dac_value);
-  drawValues();
 }
 
 // =========================================================
@@ -201,14 +121,14 @@ void config_dac(byte device_address,
                 unsigned int c3,
                 unsigned int c4) {
 
-  unsigned int tftc = 0;
-  tftc |= (c4 << 3);
-  tftc |= (c3 << 2);
-  tftc |= (c2 << 1);
-  tftc |= c1;
+  unsigned int tft = 0;
+  tft |= (c4 << 3);
+  tft |= (c3 << 2);
+  tft |= (c2 << 1);
+  tft |= c1;
 
   Wire.beginTransmission(device_address);
   Wire.write(0x01);
-  Wire.write(tftc);
+  Wire.write(tft);
   Wire.endTransmission();
 }
